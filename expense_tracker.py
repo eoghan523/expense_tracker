53acfeef3824761a51fe61c4a892ec875efb759b
eoghan523 <eoghan523@gmail.com>
2024-11-02 21:11:14 +0000

    Started to define the add expense and the view expense function

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,6 +1,29 @@
 import requests  #imports rquests module neeeded for handling http requests.
 
+BASE_URL = "http://127.0.0.1:5000/expenses"
 
-response = requests.get('https://github.com/eoghan523/expense_tracker')
-print(response.status_code)  # Status code of the response
-print(response.json())  # JSON response if available
+
+
+
+
+def add_expense(description, amount, date):    #Add Expense function which defines the add expense dictionary key-pairs.
+    new_expense = {
+        "Description": description,
+        "Amount":     amount,
+        "Date":       date
+    }
+    response = requests.post(BASE_URL, json=new_expense)
+    if response.status_code == 201:
+        print("Expense added successfully:", response.json())
+
+    else:
+        print("Error adding your response.", response.status_code)
+
+
+def view_expenses():
+    response = requests.get(BASE_URL)
+    if 
+
+
+
+
